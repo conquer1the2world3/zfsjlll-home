241841813bfdabe9c3cc3d9aaf6accc8df72e30f
zfsjlll <qwerzxcasd4@gmail.com>
2025-02-11 19:26:25 +0800

    更新导航网站

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Download, Archive, Fire, LaptopCode,Tshirt,Book } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Download, Archive, Fire, LaptopCode,Tshirt,Book,IceCream,BloggerB,Fish } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -72,6 +72,9 @@ const siteIcon = {
   LaptopCode,
   Tshirt,
   Book,
+  IceCream,
+  BloggerB,
+  Fish,
 };
 
 // 链接跳转
