cb5b4f552c9b2bd8fa870e4c8d9eb3a319a85902
zfsjlll <qwerzxcasd4@gmail.com>
2025-02-21 11:44:41 +0800

    updtae server-monitor

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Download, Archive, Fire, LaptopCode,Tshirt,Book,IceCream,BloggerB,Fish } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Download, Archive, Fire, LaptopCode,Tshirt,Book,IceCream,BloggerB,Fish,Server} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -75,6 +75,7 @@ const siteIcon = {
   IceCream,
   BloggerB,
   Fish,
+  Server
 };
 
 // 链接跳转
